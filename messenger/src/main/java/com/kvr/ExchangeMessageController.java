fc10a889333afccc2b28500148cb53170e03a3e6
vijaya raghava k <raghava.k2@gmail.com>
2020-04-05 19:28:29 +0530

    Added UI layout for sending request

---
+++ messenger/src/main/java/com/kvr/ExchangeMessageController.java
@@ -0,0 +1,18 @@
+package com.kvr;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController()
+public class ExchangeMessageController {
+
+	@GetMapping("/api/v1/s1")
+	public String get() {
+		return "hello";
+	}
+	
+	@GetMapping("/api/v1/s2")
+	public String get2() {
+		return "hello2";
+	}
+}
